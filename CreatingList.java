e5523994b8775aadfb1ba8d4a897e79583faa031
pratapshivendra817 <71538947+pratapshivendra817@users.noreply.github.com>
2024-11-18 19:59:55 +0530

    Create ArrayList Step 1

---
+++ CreatingList.java
@@ -0,0 +1,18 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class CreatingList{
+
+    public static void main(String[] args) {
+        
+        
+         // Using ArrayList
+        List<String> arrayList = new ArrayList<>();
+
+        System.err.println("List" + arrayList);
+
+    }
+
+}
