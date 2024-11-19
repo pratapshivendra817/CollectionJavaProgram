5a48823c30a15f1dc2c10c9852be213e559cfebb
pratapshivendra817 <71538947+pratapshivendra817@users.noreply.github.com>
2024-11-19 10:59:36 +0530

    Step 2

---
+++ AddElement.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class  AddElement {
+
+     public static void main(String[] args) {
+
+    
+     // Using ArrayList
+     List<String> arrayList = new ArrayList<>();
+
+     // Add element in List
+
+     arrayList.add("apple");
+     arrayList.add("cherray");
+     arrayList.add("Banana");
+     arrayList.add("Iphone");
+
+
+     System.out.println("List" + arrayList);
+
+}
+}
